1af38014acdcc705d5a08f2d730256b3631c980c
Laura <laurafurtadoamaral@gmail.com>
2024-10-18 15:22:34 -0300

    Configuração do Banco de Dados e da Tabela Veiculos

---
+++ Models/Veiculo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_dev_backend.Models
+{
+
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Placa é obrigatório")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "O campo Ano do Veículo é obrigatório")]
+        public int AnoVeiculo { get; set; }
+
+        [Required(ErrorMessage = "O campo Ano do Modelo é obrigatório")]
+        public int AnoModelo     { get; set; }
+}
+}
--- obj/Debug/net8.0/mf-dev-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/mf-dev-backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mf-dev-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d3b468bcbb2c8e8638983add4235c7a6d95eba0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mf-dev-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mf-dev-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
